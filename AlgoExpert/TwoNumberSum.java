882a9cd7c36894f0e469655c90a3665a142767e9
Zackary Wright <47463747+ZackaryWright@users.noreply.github.com>
2021-12-09 01:46:27 -0500

    Add files via upload

---
+++ AlgoExpert/TwoNumberSum.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Program {
+  public static int[] twoNumberSum(int[] array, int targetSum) {
+//  O(n) T | O(n) S
+		
+		int[] output = new int[2];
+		HashMap<Integer,Integer> map = new HashMap<>();
+		
+		for(int i = 0; i < array.length; i++) {
+			int diff = targetSum - array[i];
+			if(map.containsKey(diff)) {
+				output[0] = diff;
+				output[1] = array[i];
+				return output;
+			}
+			map.put(array[i], i);
+		}
+		
+    return new int[0];
+  }
+}
